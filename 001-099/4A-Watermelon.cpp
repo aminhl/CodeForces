92f462fe69bd7b6f882b636872f9bd0fc7a00541
aminhl <amin.hlel@esprit.tn>
2021-07-12 12:08:32 +0100

    001-099

---
+++ 001-099/4A-Watermelon.cpp
@@ -0,0 +1,12 @@
+//Author Amin Hlel
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int w = 0; cin >> w;
+    string output = (w <= 2 || w % 2) ? "NO" : "YES";
+    cout << output << endl;
+    return 0;
+}
+
---
+++ 001-099/71A-WayTooLongWords.cpp
@@ -0,0 +1,17 @@
+//Author Amin Hlel
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+   int n; cin >>n;
+   for(int i=0;i<n;i++){
+       string str; cin >>str;
+       int len=str.length();
+       if(len>10){
+           cout <<str[0]<<(len-2)<<str[len-1]<<endl;
+       }
+       else
+           cout <<str <<endl;
+   }
+}
