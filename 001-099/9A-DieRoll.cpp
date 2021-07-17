ee233ff22cdded0415944cbbb9b5520b86d4086b
aminhl <amin.hlel@esprit.tn>
2021-07-17 21:37:51 +0100

    001-099

---
+++ 001-099/9A-DieRoll.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int Y, W;
+    cin >> Y >> W;
+    const string probability[7] = {"", "1/1", "5/6", "2/3", "1/2", "1/3", "1/6"};
+    int D = max(Y, W);
+    cout << probability[D] << endl;
+}
+
+
